e20bbfb7e8a0fe1d1aade9a6f45121008bb392d0
John Oliver Amurao <44348331+joliveramu@users.noreply.github.com>
2023-08-14 22:42:27 +0800

    (m) Added get username/id end point

--- tmsauth/src/main/java/com/example/tmsauth/HomeController.java
+++ tmsauth/src/main/java/com/example/tmsauth/HomeController.java
@@ -1,7 +1,19 @@
 package com.example.tmsauth;
 
+import java.security.Principal;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController {
@@ -14,6 +26,20 @@ public class HomeController {
 	@GetMapping("/tms")
 	public String signIn()
 	{
-		return "";
+		return "<a href='/username'>To get User ID</a>";
 	}
+	
+//	@RequestMapping(value = "/username", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String currentUserName(Principal principal) {
+//        return principal.getName(); 
+//        //return principal.toString();
+//    }
+	
+	@RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserNameSimple(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        return principal.getName();
+    }
 }
